17afe18c8af22bfa407d598d085e2ff684f6792f
Mmeso1 <anisiubammesoma8@gmail.com>
2024-05-29 01:27:56 +0100

    last task for es6 basics

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,5 +1,14 @@
-import appendToEachArrayValue from "./10-loops.js";
+import createEmployeesObject from "./11-createEmployeesObject.js";
+import createReportObject from "./12-createReportObject.js";
+import createIteratorObject from "./100-createIteratorObject.js";
+import iterateThroughObject from "./101-iterateThroughObject.js";
 
-console.log(
-  appendToEachArrayValue(["appended", "fixed", "displayed"], "correctly-")
-);
+const employees = {
+  ...createEmployeesObject("engineering", ["Bob", "Jane"]),
+  ...createEmployeesObject("marketing", ["Sylvie"]),
+};
+
+const report = createReportObject(employees);
+const reportWithIterator = createIteratorObject(report);
+
+console.log(iterateThroughObject(reportWithIterator));
