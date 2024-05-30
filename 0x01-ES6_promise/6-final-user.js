6c86341eebbe90f5a07f48025a69c3f8be134eb8
Mmeso1 <anisiubammesoma8@gmail.com>
2024-05-31 00:12:36 +0100

    task 6 fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,13 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
   return Promise.allSettled(promises).then((results) => {
-    const resultArray = [];
-    for (const result of results) {
-      resultArray.push({
-        status: result.status,
-        value: result.value,
-      });
-    }
-    return resultArray;
+    results.map((r) => ({
+      status: r.status,
+      value: r.status === 'fulfilled' ? r.value : String(r.reason),
+    }));
   });
 }
