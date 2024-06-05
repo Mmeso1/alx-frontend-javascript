95acd7cb546d93d4753ef92e51c1c31f8e20b5ad
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-05 02:16:49 +0100

    yay!! it works. create a clean set[

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(aSet, startString) {
-  if (startString === "") {
-    return "";
+  if (startString === '') {
+    return '';
   }
 
   const string = [...aSet].filter((item) => item.startsWith(startString));
-  const newString = string.map((item) => item.replace(startString, ""));
-  return "".concat(newString.join("-"));
+  const newString = string.map((item) => item.replace(startString, ''));
+  return ''.concat(newString.join('-'));
 }
