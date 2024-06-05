928dceb3df975788a24534d6bc0dfadbfe6b345e
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-05 01:10:35 +0100

    upgrade student by grade based on city

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,10 +2,10 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students.map((student) => {
     const studentCopy = { ...student };
     if (studentCopy.location === city) {
-      const gradeObj = newGrades.find(
+      const gradeObjArray = newGrades.filter(
         (grade) => grade.studentId === studentCopy.id,
       );
-      studentCopy.grade = gradeObj ? gradeObj.grade : 'N/A';
+      studentCopy.grade = gradeObjArray.length > 0 ? gradeObjArray[0].grade : 'N/A';
     }
     return studentCopy;
   });
