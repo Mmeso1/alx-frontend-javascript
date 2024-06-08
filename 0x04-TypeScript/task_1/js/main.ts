691a8a2de9c09c3f67dc6ac06cd8060e0a5a1034
Mmeso1 <anisiubammesoma8@gmail.com>
2024-06-08 07:40:42 +0100

    task2, create teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,7 @@ interface Teacher {
   location: string;
   [propName: string]: any;
 }
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
---
+++ 0x04-TypeScript/task_1/js/test.ts
@@ -0,0 +1,9 @@
+import { Directors } from "./main";
+const director1: Directors = {
+  firstName: "John",
+  lastName: "Doe",
+  location: "London",
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
